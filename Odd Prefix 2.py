23324f91de1227353dca0ca8b8a8a921388fff11
Adarsh Gupta <hi@adarshgupta.dev>
2022-08-08 02:52:55 +0530

    wrote better ans with less bug

--- Odd Prefix 2.py
+++ Odd Prefix 2.py
@@ -37,58 +37,80 @@ Sample input 1:
 3
 4 3 6 
 
-Sample input 2:
-3
-0 0 1
-
-Sample input 3:
-3
-1 1 1 
-
 Sample output 1:
 3
 3 4 6
 
+Sample input 2:
+5
+1 2 3 1 1
+
 Sample output 2:
 3
-1 0 0
+1 1 1 2 3
+
+Sample input 3:
+5
+1 1 1 2 2 
 
 Sample output 3:
-1
-1
-
+4
+1 1 1 2 2
 
 """
 
 # Solution with explanation.
 
-def ans(odd, evens):
-    len_evens = len(evens)
-    len_odds = len(odds)
-    
-    if len_odds == 0:
-        print(0)
-        return
-    else:
-        print(len_evens + 1)
-        print(f"{odd[0]}", end=" ")
-        print(*evens, end=" ")
-        print(*odds[1:])
-        return
+
+def ans(arr):
+    count = 0  # we will increment count, every time we add a number that may result in odd sum.
+    # Separate the odd and even numbers, and sort the two separate arrays
+    even = []
+    odd = []
+    for nr in arr:
+        if nr % 2 == 0:
+            even.append(nr)
+        else:
+            odd.append(nr)
+
+    even.sort(reverse=True)
+    odd.sort(reverse=True)
+
+    # Initialize the array
+    ans_arr = []
+
+    # Place the lowest odd number
+    if odd:
+        ans_arr.append(odd.pop())
+        count += 1
+
+    # keep going until we have odd AND even numbers
+    while odd and even:
+        # If the next odd number is lower than the even number, and we still have 2 or more, we place 2
+        if odd[-1] < even[-1] and len(odd) >= 2:
+            ans_arr.append(odd.pop())
+            ans_arr.append(odd.pop())
+            count += 1
+        # We don't have 2 odd numbers, or the even number is lower, we place the even number
+        else:
+            ans_arr.append(even.pop())
+            count += 1
+    count += len(even) + len(odd) // 2
+
+    # NOTE: The order of the next two loops does not matter, as only 1 will run
+    # Place the remaining odd numbers
+    while odd:
+        ans_arr.append(odd.pop())
+
+    # Place the remaining even numbers
+    while even:
+        ans_arr.append(even.pop())
+
+    print(count)
+    print(ans_arr)
 
 
 n = input()
 a = [int(x) for x in input().split(" ")]
-a.sort()
 
-
-odds = []
-evens = []
-
-for i in a:
-    if i % 2:
-        odds.append(i)
-    else:
-        evens.append(i)
-
-ans(odds, evens)
+ans(arr)
