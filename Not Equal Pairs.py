9cb64e8db364d7c9e3fa675885cad5f8a2f59d64
Adarsh Gupta <hi@adarshgupta.dev>
2022-08-10 22:33:08 +0530

    Rewrote code with new logic. +97% improvement.

--- Not Equal Pairs.py
+++ Not Equal Pairs.py
@@ -32,31 +32,43 @@ In maximum number of pairs are 2.
 
 # Solution with explanation.
 
+"""
+This question seems tricky; but is not.
 
-def findPair(arr):
-    global pairs
-    temp = arr[0]
-    for i in range(1, len(arr)):
-        if arr[i] != temp:
-            pairs += 1
-            arr[0], arr[i - 1] = arr[-1], arr[-2]
-            arr.pop()
-            arr.pop()
-            return
-    return True
+So, there are N elements. They must form N // 2 pairs. 
+
+What can prevent them from doing so? 
+If some random element occur more than n/2 times. If not, ans would always will be n//2.
+
+Now, say if some random element does occur more than n/2 times.
+Then, every other element will have a pair. 
+so ans is n - frequency of majority element.
+"""
+
+
+def findPair(arr, n):
+    majority_found = False
+    majority_element, majority_frequency = 0, 0
+    mapping = {}
+    for i in arr:
+        if majority_found:
+            if i == majority_element:
+                majority_frequency += 1
+        else:
+            if i in mapping:
+                mapping[i] += 1
+            else:
+                mapping[i] = 1
+            if mapping[i] > n // 2:
+                majority_found = True
+                majority_element = i
+                majority_frequency = mapping[i]
+    if majority_found:
+        return n - majority_frequency
+    return n // 2
 
 
 n = int(input())
 arr = [int(x) for x in input().split(" ")]
 
-pairs = 0
-
-while len(arr) > 1:
-    if findPair(arr):
-        break
-
-if len(arr) > 1:
-    if arr.count(arr[0]) <= n - len(arr):
-        pairs += arr.count(arr[0]) // 2
-
-print(pairs)
+print(findPair(arr, n))
