5e42808ee2ac07bc241bf02ad3a856f76601b6e0
Adarsh Gupta <hi@adarshgupta.dev>
2022-08-10 09:43:52 +0530

    solved perfectly filled buckets

--- Perfectly Filled Buckets.py
+++ Perfectly Filled Buckets.py
@@ -49,4 +49,48 @@ so min(B1, B2) = 2
 Note: 
 We have maximize size smaller of both B1, B2; to have bigger min of both. 
 We can't add move 2 from b2 to b1 because: then b1 would not be perfectly filled and b2 size would become 1, reducing min value.
+For same, reason we can't move 3 from b2 to b1.
+We can move 5, but there won't be any chance to result.
 """
+
+# Solution with explanation.
+
+# Since does ask, what buckets will contain; it is idiotic to calculate all combinations.
+# we will calculate possible length of sub-arrays. i.e max of min of bucket lengths.
+def solve(Arr, length, buckets):
+    if buckets > length // 2:
+        return 1
+    if buckets == 1:
+        return length
+
+    Arr.sort()
+    singles, pairs, ans = 0, 0, 0
+
+    i = 0
+    while i < length:
+        if i == length - 1 or Arr[i] != Arr[i + 1]:
+            singles += 1
+            i += 1
+        else:
+            pairs += 1
+            i += 2
+
+    while pairs:
+        if pairs >= buckets:
+            ans += 2
+            pairs -= buckets
+
+    if singles >= buckets:
+        ans += 1
+        singles -= 1
+
+    return ans
+
+
+Testcases = int(input())
+
+for i in range(Testcases):
+    N, B = [int(x) for x in input().split(" ")]
+    A = [int(x) for x in input().split(" ")]
+
+    print(solve(A, N, B))
