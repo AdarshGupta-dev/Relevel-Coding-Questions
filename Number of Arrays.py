a4df711215014c3d00b939962442a60ad091a75e
Adarsh Gupta <hi@adarshgupta.dev>
2022-08-11 09:46:27 +0530

    solved number of arrays

--- Number of Arrays.py
+++ Number of Arrays.py
@@ -26,13 +26,24 @@ Number of array % 10^9+7
 
 Sample input:
 2
-2 3  
+3 2  
 
 Sample output:
 4
 
 Explanation: 
 
+Length of 32 is 2. The number of digits in 32 is 2. So, new numbers should be of 2 digits only.
+31 is a valid ans. As 31 ≤ 32; and 3≤3, and 1≤2. New number 31 is less than original. Also, every new digit is less or equal to the original digit.
+
+Similarly, 11, 12, 21, 22, 31, and 32 are valid of condition 2nd. 14, 41, 33, and 15 are not valid.
+Now we also have to check for repeating numbers and reject them. So, 11 and 22 from previous step, fails condition 3.
+
+Final ans would be [12, 21, 31, 32] i,e. 4 counts.
+
+For 45: [12, 13, 14, 15, 21, 23, 24, 25, 31, 32, 34, 35, 41, 42, 43, 45]: Ans would be 16
+
+Thank you.
 1 can be added at position 1, 2, or 3 to make it into [2, 1, 3, 1]. 
 This can be divided into [2], [1], and [3, 1]; which does satisfy 2^k form.
 
@@ -40,3 +51,18 @@ This can be divided into [2], [1], and [3, 1]; which does satisfy 2^k form.
 """
 
 # Solution with explanation.
+
+
+def solve(arr, length):
+    ans = 1
+    for i in arr:
+        length -= 1
+        ans *= i - length
+    return ans % (10**9 + 7)
+
+
+n = int(input())
+A = [int(x) for x in input().split(" ")]
+A.sort(reverse=True)
+
+print(solve(A, n))
