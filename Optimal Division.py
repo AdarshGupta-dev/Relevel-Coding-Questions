993edad38d948bff3440b5d4e29870b5f27a067b
Adarsh Gupta <hi@adarshgupta.dev>
2022-08-11 19:43:07 +0530

    Added a new question

---
+++ Optimal Division.py
@@ -0,0 +1,42 @@
+"""
+Question Name: Optimal division
+
+You are given Array A consisting of N positive integers.
+
+Value of an array is defined as number of unique elements it have. 
+Like value of array [1,2,1,2,2,3] = 3 as it has 3 unique elements(1,2 and 3), value of array [1,2,3,4] = 4 and so on.
+
+Split the array into 2 parts, such that sum of values of 2 sub-arrays are maximum. 
+
+Note : It may be possible that one of them does not receive any element of the bought array.
+
+Input Format
+
+First line contains a single integer denoting N.
+Next line contains N space separated integers denoting the elements of the bought array.
+
+Output Format
+
+Print the maximum possible total value of the final arrays that can be achieved if elements of the array are dived optimally.
+
+Constraints
+
+1<=N<=10^5
+1<= element of the array<=10^9
+
+Sample Input 1
+6
+1 2 1 2 2 3
+
+Sample Output 1
+5
+
+Explanation of Sample 1
+You can divide the array elements as :
+A1 = [1,2,2], value of array = 2
+A2 = [2,1,3], value of array = 3
+Hence total value = 2+3 = 5. 
+
+It can be proved that this is the maximum possible value.
+"""
+
