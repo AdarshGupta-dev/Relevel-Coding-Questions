fc5918b5a723e70f012c5afba99f8d2a8511da7a
Adarsh Gupta <hi@adarshgupta.dev>
2022-08-16 09:31:02 +0530

    solved Powerful Triplet

--- Powerful-Triplet.py
+++ Powerful-Triplet.py
@@ -35,3 +35,32 @@ Sample Output 1
 Explanation of Sample 1
 If she choses the triplet (1, 3, 4), then power = | A1 - A3 | * A4 = | 1 - 4 | * 3 = 9
 """
+t = int(input())
+output = []
+
+for i in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    ans = 0
+
+    left_small = left_big = a[0]
+
+    right_big = max(a[1:])
+
+    for i in range(1, n - 1):
+        if a[i - 1] > left_big:
+            left_big = a[i - 1]
+        if a[i - 1] < left_small:
+            left_small = a[i - 1]
+
+        if a[i] == right_big:
+            right_big = max(a[i + 1 :])
+
+        temp = max(abs(left_small - a[i]), abs(left_big - a[i])) * right_big
+        ans = max(ans, temp)
+
+    output.append(ans)
+
+
+print(*output, sep="\n")
